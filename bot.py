7c165684f337f91d4fd6417ebd31f678183df5c5
achi996 <akmtvchyngyz@gmail.com>
2021-02-10 00:07:15 +0600

    start

---
+++ bot.py
@@ -0,0 +1,23 @@
+import telebot
+import random
+
+
+bot = telebot.TeleBot('1691834307:AAHhePEkTOE9nEboafg-iUpTdVm5fX9QFvQ')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, random.choice(['Мал базар']))
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower():
+        a = [
+            'Гармон жун', 'Алипекарь', 'Боботик, чтоб не болел животик', 
+            'Кашат мал', 'Кудивер шланг', 'Семен алкаш', 'Жумадрыщь', 'Атайчик-зайчик',
+            'Рыся-перзидент', 'Жози-ит', 'Азат-тоок', 'Нура-крокодайл', 'Пекчо-последний',
+            'Тети-транс', 'Тока-бандит', 'Дава-нигер', 'Чика-канчика', 'Мофоня чайнавачы',
+            'Вася-пидор',
+            ]
+        bot.send_message(message.chat.id, random.choice(a))
+
+bot.polling(none_stop=True)
